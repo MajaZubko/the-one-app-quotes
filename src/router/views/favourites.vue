bb23059e3959e34f959c4fed32b76ba3b6a32a18
MajaZubko <majamzubko@gmail.com>
2021-05-18 15:08:36 +0200

    Create favourites route

---
+++ src/router/views/favourites.vue
@@ -0,0 +1,18 @@
+<script>
+import appConfig from '@src/app.config'
+import Layout from '@layouts/main.vue'
+
+export default {
+  page: {
+    title: 'Favourites',
+    meta: [{ name: 'description', content: appConfig.description }],
+  },
+  components: { Layout },
+}
+</script>
+
+<template>
+  <Layout>
+    Hello
+  </Layout>
+</template>
